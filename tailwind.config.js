5b63825278b9b0ed18deb2cb007305e09bdea201
Ole <oleh.kodehode@gmail.com>
2023-02-19 21:22:36 +0100

    .

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'greyText': '#c2ccd8',
+      'pinkText': '#e16bab',
+      'blueText': '#73c0e7',
+      'cardBG': '#1e293b'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
