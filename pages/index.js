9dc370573427fc7ac082421dd1c5ebe73c30ad20
Ole Håvard Furuseth Bergan <oleh.kodehode@gmail.com>
2023-03-07 13:05:17 +0100

    linked mine-erfaringer to button on landingpage

--- pages/index.js
+++ pages/index.js
@@ -58,120 +58,124 @@ export default function Home() {
         </h2>
       </header>
       <main
-      className="mx-auto max-w-7xl"
+        className="mx-auto max-w-7xl"
       >
         <div
           className="justify-around md:flex"
         >
           <section
-            
+
           >
             <div
-            className="md:mx-0 block w-64 py-6 px-8 mx-auto bg-cardBG text-greyText rounded-[20px] shadow-[0px_0_10px_rgba(0,0,0,0.8)] max-h-[420px]"
+              className="md:mx-0 block w-64 py-6 px-8 mx-auto bg-cardBG text-greyText rounded-[20px] shadow-[0px_0_10px_rgba(0,0,0,0.8)] max-h-[420px]"
             >
 
-            <p>
-              &lt;
-              <span
-                className=" text-pinkText"
+              <p>
+                &lt;
+                <span
+                  className=" text-pinkText"
                 >
-                Developer
-              </span>
-            </p>
-            <p
-              className="pl-3 "
+                  Developer
+                </span>
+              </p>
+              <p
+                className="pl-3 "
               >
-              id=
-            </p>
-            <p
-              className="pl-3 text-blueText"
+                id=
+              </p>
+              <p
+                className="pl-3 text-blueText"
               >
-              "Ole Håvard
-            </p>
-            <p
-              className="pl-3 text-blueText"
+                "Ole Håvard
+              </p>
+              <p
+                className="pl-3 text-blueText"
               >
-              Furuseth Bergan"
-            </p>
-            <p
-              className="pl-3"
+                Furuseth Bergan"
+              </p>
+              <p
+                className="pl-3"
               >
-              className=
-            </p>
-            <p
-              className="pl-3 text-blueText"
+                className=
+              </p>
+              <p
+                className="pl-3 text-blueText"
               >
-              "Frontend Developer"
-            </p>
-            <p>&gt;</p>
-            <p className="pl-3">
-              &lt;
-              <span
-                className=" text-pinkText"
+                "Frontend Developer"
+              </p>
+              <p>&gt;</p>
+              <p className="pl-3">
+                &lt;
+                <span
+                  className=" text-pinkText"
                 >
-                img
-              </span>
-              &gt;
-            </p>
-            <Image
-              className="pl-6 drop-shadow-[0px_0_20px_rgba(20,205,200,0.4)]"
-              src={Selfie}
-              width={146}
+                  img
+                </span>
+                &gt;
+              </p>
+              <Image
+                className="pl-6 drop-shadow-[0px_0_20px_rgba(20,205,200,0.4)]"
+                src={Selfie}
+                width={146}
               />
-            <p
-              className="pl-3"
+              <p
+                className="pl-3"
               >
-              &lt;/
-              <span
-                className=" text-pinkText"
+                &lt;/
+                <span
+                  className=" text-pinkText"
                 >
-                img
-              </span>
-              &gt;
-            </p>
-            <p>
-              &lt;/&nbsp;
-              <span
-                className=" text-pinkText"
+                  img
+                </span>
+                &gt;
+              </p>
+              <p>
+                &lt;/&nbsp;
+                <span
+                  className=" text-pinkText"
                 >
-                Developer
-              </span>
-              &gt;
-            </p>
+                  Developer
+                </span>
+                &gt;
+              </p>
             </div>
             <div
               className="flex flex-col items-center gap-4 pt-10 text-[#000000] h-"
-              >
+            >
               <button
                 className="w-[152px] h-10 rounded-2xl shadow-[0px_0px_40px_rgba(55,59,61,0.4)]"
-                >
+              >
                 Om meg
               </button>
               <Link
-              href="/mine-prosjekter"
+                href="/mine-prosjekter"
               >
-              <button
-                className="w-[152px] h-10  rounded-2xl shadow-[0px_0px_40px_rgba(55,59,61,0.4)]"
+                <button
+                  className="w-[152px] h-10  rounded-2xl shadow-[0px_0px_40px_rgba(55,59,61,0.4)]"
                 >
-                Mine prosjekter
-              </button>
-                  </Link>
-              <button
-                className="w-[152px] h-10 rounded-2xl shadow-[0px_0px_40px_rgba(55,59,61,0.4)]"
+                  Mine prosjekter
+                </button>
+              </Link>
+              <Link
+                href="/mine-erfaringer"
+              >
+                <button
+                  className="w-[152px] h-10 rounded-2xl shadow-[0px_0px_40px_rgba(55,59,61,0.4)]"
                 >
-                Mine erfaringer
-              </button>
+                  Mine erfaringer
+                </button>
+              </Link>
             </div>
           </section>
           <section
             className="hidden w-1/2 md:block"
-            >
+          >
             <div
               className=" border rounded-tl-[20px] rounded-tr-[20px] bg-cardBG"
-              >
+            >
               <h2
                 className="my-4 text-2xl text-center text-pinkText"
-                >
+              >
                 Noen av mine prosjekter
               </h2>
               <nav
