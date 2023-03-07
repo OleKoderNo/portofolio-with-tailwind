9c55dfe4afe40b6fb995a0ce4ef339edf149ca8f
Ole Håvard Furuseth Bergan <oleh.kodehode@gmail.com>
2023-03-07 09:28:44 +0100

    jkhkjhjk

--- components/layout.js
+++ components/layout.js
@@ -5,12 +5,10 @@ export default function Layout({ children }) {
 	return (
 		<>
 		<div
-		// site
 		className="flex flex-col min-h-screen"
 		>
 		 <NavBar />
 		 <div
-		//  content
 		className="flex-1"
 		 >
 		 {children}
